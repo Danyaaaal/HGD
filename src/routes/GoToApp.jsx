b3db872c69d5ca29bafd90e922ae0678f6295e96
Dany <danialikhani@gmail.com>
2024-02-22 12:01:41 +0100

    first step of style

--- src/routes/GoToApp.jsx
+++ src/routes/GoToApp.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import tts from "../assets/texttospeech.png"
-import stt from "../assets/speechtotext.png"
+import tts from "../assets/texttospeech.png";
+import stt from "../assets/speechtotext.png";
 function GoToApp() {
   return (
     <>
-      <h1>Text to speech & Speech to text</h1>
-
-      <ul>
-        <li>
-          <Link to="/app/text-to-speech"><img src={tts} alt="text to speech icon"/></Link>
-        </li>
-        <li>
-          <Link to="/app/speech-to-text"><img src={stt} alt="speech to text icon"/></Link>
-        </li>
-      </ul>
+      <div>
+        <h1>Text to speech & Speech to text</h1>
+      </div>
+      <div>
+        <Link to="/app/text-to-speech">
+          <img src={tts} alt="text to speech icon" />
+        </Link>
+      </div>
+      <div>
+        <Link to="/app/speech-to-text">
+          <img src={stt} alt="speech to text icon" />
+        </Link>
+      </div>
     </>
   );
 }
